a08e60a0e4556ca1d3b7e8fbef1cca8e7bce6eab
Alfie <alfielenton@gmail.com>
2025-12-29 14:53:04 +0000

    Creating mapping between images and labels

---
+++ data_handling.py
@@ -0,0 +1,48 @@
+import os
+import cv2
+import numpy as np
+
+def get_labels(file_path):
+
+    with open(file_path, 'r') as f:
+        line = f.readline()[:-1]
+
+    line += ' '
+    values = []
+    i_start = 0
+    for i, n in enumerate(line):
+        if n == ' ':
+            values.append(line[i_start:i])
+            i_start = i
+
+    values = [float(v) for v in values]
+    return values
+
+def get_image(file_path):
+    return cv2.imread(file_path)
+
+
+folder_names = ['train', 'valid', 'test']
+
+image_filenames = {}
+label_filenames = {}
+
+for name in folder_names:
+    image_filenames[name] = os.listdir('dataset//' + name + '//images')
+    label_filenames[name] = os.listdir('dataset//' + name + '//labels')
+
+images = {}
+labels = {}
+
+for name in folder_names:
+
+    images[name] = {}
+    for image_name in image_filenames[name]:
+        im = get_image('dataset//' + name + '//images//' + image_name)
+        images[name][image_name[:-4]] = (im, im.shape)
+
+    labels[name] = {}
+    for label_name in label_filenames[name]:
+        la = get_labels('dataset//' + name + '//labels//' + label_name)
+        labels[name][label_name[:-4]] = la
+
