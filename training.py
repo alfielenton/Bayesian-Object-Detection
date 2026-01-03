01119923f86e92ea112590b9308d076c718d9894
Alfie <alfielenton@gmail.com>
2026-01-03 19:49:58 +0000

    Making training file

---
+++ training.py
@@ -0,0 +1,7 @@
+import torch
+from torch.utils.data import DataLoader
+import numpy as np
+
+from network_components import classifier, regresser, network
+from data_handlings import data_handling_functions
+
