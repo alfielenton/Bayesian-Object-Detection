15306da2831b015f5a41d5c1cbfc6e02fd7d60e1
Alfie <alfielenton@gmail.com>
2026-01-03 17:58:06 +0000

    First draft Bayes Regresser

--- network.py
+++ network.py
@@ -110,7 +110,7 @@ class BayesClassifier:
     
     def direction_log_nu_Mu(self, batch):
 
-        fts, cs = batch
+        _, cs = batch
         nu_Sig = torch.exp(self.log_nu_Sig)
         alpha = torch.exp(-self.lambda_Sig + (nu_Sig**2)/2)
 
@@ -144,4 +144,30 @@ class BayesClassifier:
                 -.5 * ((xs[..., None] - Mu) * (1/Sig) * (xs[..., None] - Mu)).sum(dim=1)
         
         log_p = log_p - torch.log(torch.exp(log_p).sum(dim=1))
-        return log_p.argmax(dim=1)
+        return log_p
+    
+class BayesRegresser:
+
+    def __init__(self, device, n_feats, n_labels, sig_W, sig_y, lr):
+
+        self.device = device
+        self.N = n_feats
+        self.M = n_labels
+        self.sig_W = sig_W
+        self.sig_y = sig_y
+        self.lr = lr
+
+        self.W = torch.tensor(np.random.rand(self.M, self.N) * self.sig_W, dtype=torch.float32, device = self.device)
+
+    def weight_posterior(self, batch):
+
+        X, Y = batch
+        Y_T_X = Y.T @ X
+        X_T_X = X.T @ X
+
+        mu_W = torch.linalg.solve(X_T_X + (self.sig_y/self.sig_W)**2 * torch.eye(self.N), Y_T_X.T).T
+        return mu_W
+    
+    def parameters_step(self, batch):
+        weight_diff = self.weight_posterior(batch) - self.W
+        self.W = self.W + self.lr * weight_diff
