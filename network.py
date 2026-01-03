bd355ae32da9bd68193f23a476f9d354d6737537
Alfie <alfielenton@gmail.com>
2026-01-03 18:01:29 +0000

    Including device and changing symbol names in bayes classifier

--- network.py
+++ network.py
@@ -65,21 +65,22 @@ class CNNhead(nn.Module):
 
 class BayesClassifier:
 
-    def __init__(self, n_feats, n_cats, sig_Mu, sig_Sig, lr):
+    def __init__(self, device, n_feats, n_cats, sig_Mu, sig_Sig, lr):
 
         self.lr = lr
 
+        self.device = device
         self.C = n_cats
         self.N = n_feats
 
         self.sig_Mu = sig_Mu
         self.sig_Sig = sig_Sig
 
-        self.log_nu_Sig = torch.tensor(np.random.rand())
-        self.log_nu_Mu = torch.tensor(np.random.rand())
+        self.log_nu_Sig = torch.tensor(np.random.rand(), device = self.device, dtype = torch.float32)
+        self.log_nu_Mu = torch.tensor(np.random.rand(), device = self.device, dtype = torch.float32)
 
-        self.lambda_Sig = torch.tensor(np.random.rand(self.N, self.C))
-        self.lambda_Mu = torch.tensor(np.random.rand(self.N, self.C)) * self.sig_Mu
+        self.lambda_Sig = torch.tensor(np.random.rand(self.N, self.C), device = self.device, dtype = torch.float32) * self.sig_Sig
+        self.lambda_Mu = torch.tensor(np.random.rand(self.N, self.C), device = self.device, dtype = torch.float32) * self.sig_Mu
 
     def predict_Mu_Sig(self):
         nu_Sig = torch.exp(self.log_nu_Sig)
@@ -87,47 +88,47 @@ class BayesClassifier:
     
     def direction_lambda_Mu(self, batch):
 
-        fts, cs = batch
+        X, Y = batch
         nu_Sig = torch.exp(self.log_nu_Sig)
         alpha = torch.exp(-self.lambda_Sig + (nu_Sig**2)/2)
 
         mult = torch.zeros_like(self.lambda_Mu)
-        mult.index_add_(1, cs, (fts.T - self.lambda_Mu[:, cs]))
+        mult.index_add_(1, Y, (X.T - self.lambda_Mu[:, Y]))
 
         return alpha * mult - self.lambda_Mu / (self.sig_Mu ** 2)
     
     def direction_lambda_Sig(self, batch):
 
-        fts, cs = batch
+        X, Y = batch
         nu_Sig = torch.exp(self.log_nu_Sig)
         alpha = torch.exp(-self.lambda_Sig + (nu_Sig**2)/2)
 
         nu_Mu = torch.exp(self.log_nu_Mu)
 
         mult = torch.zeros_like(self.lambda_Sig)
-        mult.index_add_(1, cs, (1 - ((fts.T - self.lambda_Mu[:, cs]) ** 2 + nu_Mu ** 2) * alpha[:, cs]))
+        mult.index_add_(1, Y, (1 - ((X.T - self.lambda_Mu[:, Y]) ** 2 + nu_Mu ** 2) * alpha[:, Y]))
         return -.5 * mult - self.lambda_Sig / (self.sig_Sig ** 2) - 2
     
     def direction_log_nu_Mu(self, batch):
 
-        _, cs = batch
+        _, Y = batch
         nu_Sig = torch.exp(self.log_nu_Sig)
         alpha = torch.exp(-self.lambda_Sig + (nu_Sig**2)/2)
 
         nu_Mu = torch.exp(self.log_nu_Mu)
 
-        grad = -nu_Mu * alpha[:, cs].sum() - (self.N * self.C * nu_Mu) / (self.sig_Mu ** 2) - (self.N * self.C) / nu_Mu
+        grad = -nu_Mu * alpha[:, Y].sum() - (self.N * self.C * nu_Mu) / (self.sig_Mu ** 2) - (self.N * self.C) / nu_Mu
         return nu_Mu * grad
     
     def direction_log_nu_Sig(self, batch):
 
-        fts, cs = batch
+        X, Y = batch
         nu_Sig = torch.exp(self.log_nu_Sig)
         alpha = torch.exp(-self.lambda_Sig + (nu_Sig**2)/2)
 
         nu_Mu = torch.exp(self.log_nu_Mu)
 
-        grad = -.5 * nu_Sig * (((fts.T - self.lambda_Mu[:, cs]) ** 2 + nu_Mu ** 2) * alpha[:, cs]).sum() - self.N * self.C * nu_Sig/(self.sig_Sig ** 2) - self.N * self.C / nu_Sig
+        grad = -.5 * nu_Sig * (((X.T - self.lambda_Mu[:, Y]) ** 2 + nu_Mu ** 2) * alpha[:, Y]).sum() - self.N * self.C * nu_Sig/(self.sig_Sig ** 2) - self.N * self.C / nu_Sig
         return nu_Sig * grad
     
     def parameters_step(self, batch):
