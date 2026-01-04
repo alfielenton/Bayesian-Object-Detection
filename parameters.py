bbd2f5cfcef1de5b2d5e58318dfd07dec8066a19
Alfie <alfielenton@gmail.com>
2026-01-04 19:31:40 +0000

    Training prep

--- parameters.py
+++ parameters.py
@@ -4,11 +4,17 @@ class Parameters:
     def __init__(self):
 
         self.device = 'cuda'
+        self.n_epochs = 20
         self.batch_size = 32
+        self.feat_dim = 512
+        self.box_dim = 4
+        self.n_cats = 54
 
         self.sig_Sig_c = 1.
         self.sig_Mu_c = 1.
         self.lr_c = 1e-4
+        self.lr_r = 1e-4
+        self.lr_n = 1e-4
 
         self.sig_W_r = 1.
         self.sig_y_r = 1.
--- training.py
+++ training.py
@@ -1,7 +1,39 @@
+import json
 import torch
-from torch.utils.data import DataLoader
-import numpy as np
+from torch import optim
+from torch.utils.data import DataLoader, WeightedRandomSampler
 
+from parameters import Parameters
 from network_components import classifier, regresser, network
 from data_handlings import data_handling_functions
 
+opt = Parameters()
+
+with open('dataset//train//measurements.json','r') as f:
+    meas = json.load(f)['category_count']
+
+with open('dataset//ids.json', 'r') as f:
+    ids = json.load(f)
+
+with open('dataset//categories.json', 'r') as f:
+    categories = json.load(f)['categories']
+
+train_ids = ids['train']
+valid_ids = ids['valid']
+test_ids = ids['test']
+
+print('Making training sampler...')
+labels = [data_handling_functions.get_labels(id)[0] for id in train_ids]
+weights = torch.tensor([1 / meas[categories[c]] for c in labels])
+weights /= weights.sum()
+
+sampler = WeightedRandomSampler(weights, num_samples=weights.size(0), replacement = True)
+train_dl = DataLoader(train_ids, batch_size = opt.batch_size, shuffle = False)
+print('Data loader and sampler created.\n')
+
+print('Getting models...')
+net = network.CNNhead().to(opt.device)
+net_optim = optim.Adam(net.parameters(), opt.lr_n, weight_decay = 1e-8)
+bc = classifier.BayesClassifier(opt.device, opt.feat_dim, opt.n_cats, opt.sig_Mu_c, opt.sig_Sig_c, opt.lr_c)
+br = regresser.BayesRegresser(opt.device, opt.feat_dim, opt.box_dim, opt.sig_W_r, opt.sig_y_r, opt.lr_r)
+print('Models obtained\n')
