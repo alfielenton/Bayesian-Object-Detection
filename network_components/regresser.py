518a25273e74cd7e463d2d7fa90ca0accddaa5c6
Alfie <alfielenton@gmail.com>
2026-01-05 15:50:42 +0000

    Regresser fixed

--- network_components/regresser.py
+++ network_components/regresser.py
@@ -13,34 +13,37 @@ class BayesRegresser:
         self.lr = lr
 
         self.Mu_W = torch.zeros(self.M, self.N, dtype = torch.float32, device = self.device)
-        self.inv_Sig_W = (1/self.sig_W ** 2) * torch.eye(self.N, dtype = torch.float32, device = self.device)
-
-    def weight_posterior(self, batch):
-
-        X, Y = batch
-        Y_T_X = Y.T @ X
-        X_T_X = X.T @ X
-        inv_Sig_W = X_T_X + (self.sig_y/self.sig_W)**2 * torch.eye(self.N, device=self.device)
-        inv_Sig_W = .5 * (inv_Sig_W + inv_Sig_W.T)
-
-        Mu_W = torch.linalg.solve(inv_Sig_W, Y_T_X.T).T
-        inv_Sig_W = inv_Sig_W / self.sig_y ** 2
-
-        return Mu_W, inv_Sig_W
+        self.inv_Sig_W = ((1/self.sig_W ** 2) * torch.eye(self.N, dtype = torch.float32, device = self.device)).expand(self.M, self.N, self.N)
     
     def parameters_step(self, batch):
+        X, Y = batch
 
-        Mu_W, inv_Sig_W = self.weight_posterior(batch)
+        XtX = X.T @ X           
+        YtX = Y.T @ X          
 
-        self.Mu_W = Mu_W
-        self.inv_Sig_W = inv_Sig_W
+        inv_Sig_W_prior = self.inv_Sig_W
+
+        inv_Sig_W_post = inv_Sig_W_prior + (1 / self.sig_y ** 2) * XtX
+        inv_Sig_W_post = 0.5 * (inv_Sig_W_post + torch.transpose(inv_Sig_W_post, 1, 2))
+
+        rhs = (inv_Sig_W_prior * self.Mu_W[:, None, :]).sum(dim=1) + (1 / self.sig_y ** 2) * YtX
+
+        self.Mu_W = torch.linalg.solve(inv_Sig_W_post, rhs[..., None]).squeeze(-1)
+        self.inv_Sig_W = inv_Sig_W_post
+
 
     def predict_boxes(self, X):
-        
-        mu_y = self.Mu_W @ X.T
-        sig_y = self.sig_y ** 2 * (1 + (X * torch.linalg.solve(self.inv_Sig_W, X.T).T).sum(dim=1))
+
+        mu_y = (self.Mu_W @ X.T).T 
+
+        X_exp = X.T.unsqueeze(0).expand(self.M, -1, -1)
+
+        tmp = torch.linalg.solve(self.inv_Sig_W, X_exp)
+        quad = (X_exp * tmp).sum(dim=1)
+
+        var_y = self.sig_y ** 2 * (1 + quad.T)
 
         I = torch.eye(self.M, device=self.device).expand(X.size(0), self.M, self.M)
-        sig_y = sig_y[:, None, None] * I
+        sig_y = var_y[:, None, :] * I
 
-        return mu_y.T, sig_y
+        return mu_y, sig_y
