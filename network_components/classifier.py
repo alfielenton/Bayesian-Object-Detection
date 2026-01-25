d4546671c773bcc810025b68a186ba5e33dfe2cd
Alfie <alfielenton@gmail.com>
2026-01-25 19:10:38 +0000

    Updated classifier, may need to check the maths again

--- network_components/classifier.py
+++ network_components/classifier.py
@@ -30,7 +30,7 @@ class BayesClassifier:
     def predict_cats(self, x):
 
         M = self.Mq
-        Sigma = (torch.exp(self.log_epsq) / torch.exp(self.log_tauq)) * (1 + torch.exp(self.log_Uq + self.log_Vq))
+        Sigma = (torch.exp(self.log_epsq) / (torch.exp(self.log_tauq) - 1)) * (1 + torch.exp(self.log_Uq + self.log_Vq))
         nu = 2 * torch.exp(self.log_tauq)
 
         log_p = torch.lgamma((nu + self.N) * .5) - torch.lgamma(.5 * nu) \
@@ -45,11 +45,11 @@ class BayesClassifier:
         x_d, C_d = batch
         D = x_d.size(0)
 
-        exp_log_q_M = -.5 * (self.N * self.C) * (1 + math.log(2 * torch.pi)) \
-                      - self.C * self.log_Uq - self.N * self.log_Vq
+        exp_log_q_M = .5 * (self.N * self.C) * (1 + math.log(2 * torch.pi)) \
+                      + .5 * self.C * self.log_Uq + .5 * self.N * self.log_Vq
         
-        exp_log_q_sig = -torch.lgamma(torch.exp(self.log_tauq)) - self.log_epsq + \
-                    (torch.exp(self.log_tauq) + 1) * torch.digamma(torch.exp(self.log_tauq)) - torch.exp(self.log_tauq)
+        exp_log_q_sig = torch.lgamma(torch.exp(self.log_tauq)) + self.log_epsq - \
+                    (torch.exp(self.log_tauq) + 1) * torch.digamma(torch.exp(self.log_tauq)) + torch.exp(self.log_tauq)
         exp_log_q_sig = exp_log_q_sig.sum()
 
         exp_log_q = exp_log_q_M + exp_log_q_sig
@@ -58,7 +58,7 @@ class BayesClassifier:
         tr_Up_inv_Uq = self.N * (torch.exp(self.log_Uq * 2) / self.Up ** 2)
         tr_Vp_inv_vq = self.N * (torch.exp(2 * self.log_Vq) / self.Vp ** 2)
 
-        exp_log_p_M = -.5 * self.N * self.C * math.log(2 * torch.pi) - self.C * self.log_Uq - self.N * self.log_Vq \
+        exp_log_p_M = -.5 * self.N * self.C * math.log(2 * torch.pi) - .5 * self.C * self.log_Uq - .5 * self.N * self.log_Vq \
                       -.5 * (tr_Up_inv_Vp_inv_Mq2 + tr_Up_inv_Uq * tr_Vp_inv_vq)
         
         exp_log_p_sig = self.C * (self.alpha * torch.log(self.beta) - torch.lgamma(self.alpha)) \
@@ -76,8 +76,8 @@ class BayesClassifier:
         sq_dist = ((x_d - M_cd)**2).sum(dim=1)
 
         exp_log_like = -D * math.log(self.C) -.5 * D * self.N * math.log(2 * torch.pi) \
-                       -self.N * (n_c * (self.log_epsq - torch.digamma(torch.exp(self.log_tauq)))).sum() \
-                       -.5 * (((tau_cd * (tau_cd + 1)) / eps_cd ** 2) * (sq_dist + self.N * torch.exp(self.log_Uq) * torch.exp(self.log_Vq))).sum()
+                       -.5 * self.N * (torch.log(eps_cd) - torch.digamma(tau_cd)).sum() \
+                       -.5 * ((tau_cd / eps_cd) * (sq_dist + self.N * torch.exp(self.log_Uq + self.log_Vq))).sum()
         
         exp_log_p = exp_log_like + exp_log_p_M + exp_log_p_sig
 
@@ -85,6 +85,3 @@ class BayesClassifier:
         self.optimiser.zero_grad()
         loss.backward()
         self.optimiser.step()
-
-        with torch.no_grad():
-            self.log_tauq.clamp(min=torch.tensor(1e-5, device=self.device))
